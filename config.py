117ebc0985ca840ea2e811675c8c8af1066e2f55
ZADKIEL <116737841+Moca96@users.noreply.github.com>
2023-10-10 00:21:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from os import getenv
 
 API_ID = int(os.environ.get("APP_ID", "24124040"))
 API_HASH = os.environ.get("API_HASH", "5929bf41994375da3582c5e865b5c9b2")
-BOT_TOKEN = "6570457592:AAHX7cAIs2t0tpq9vm7dbdkQttX83MzwBw4"
+BOT_TOKEN = "6570457592:AAG7PROkNmscilNLA3_JqxWnUF72ilDpf8Y"
 SUDO_USER = list(map(int, getenv("SUDO_USER", "5842773519").split()))
