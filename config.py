2cba722cde0e480120c2d7b0fac6177a542fdd23
ZADKIEL <116737841+Moca96@users.noreply.github.com>
2023-11-28 12:56:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 from os import getenv
 
-API_ID = int(os.environ.get("APP_ID", "24124040"))
-API_HASH = os.environ.get("API_HASH", "5929bf41994375da3582c5e865b5c9b2")
-BOT_TOKEN = "6570457592:AAG7PROkNmscilNLA3_JqxWnUF72ilDpf8Y"
+API_ID = int(os.environ.get("APP_ID", "28074580"))
+API_HASH = os.environ.get("API_HASH", "902a80b6c54c4cbd58e240ee241cfcbf")
+BOT_TOKEN = "6570457592:AAFVMTIwP-sDoQPfaE8ZYXWBFPWwxmOsPMQ"
 SUDO_USER = list(map(int, getenv("SUDO_USER", "5842773519").split()))
